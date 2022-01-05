7958637733434dac3e0d7ed433c522b4e4a47395
jiihyee <jiiihyeee98@gmail.com>
2022-01-05 14:56:01 +0900

    첫 커밋

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+
+ <Modal v-if ="showModal" @close="showModal = false">
+   <h3 slot="header">
+     경고!
+     <i class="closeModalBtn fas fa-times" @click="showModal = flase"></i>
+   </h3>
+    <h3 slot="body">
+     아무것도 입력하지 않으셨습니다.
+   </h3>
+
+ </Modal>
+  </div>
+</template>
+
+
+<script>
+import Modal from './common/Modal.vue'
+
+export default {
+  data: function() {
+    return {
+      newTodoItem: '',
+      showModal:false
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newTodoItem !== '') {
+        this.$emit('addItem', this.newTodoItem);
+        this.clearInput();
+      }
+      else{
+        this.showModal = !this.showModal;   //true false 를 변환해주는 부분 
+      }
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
+    }
+  },
+  components:{
+    Modal: Modal
+  }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn{
+  color: #42b983;
+}
+</style>
